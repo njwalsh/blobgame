4adb9558dcaf815dcd005d4a00d139fa0d531bae
njwalsh <nickjameswalsh1@gmail.com>
2014-05-28 21:59:34 -0700

    added test script for blob animation

---
+++ blobgame/Assets/Scripts/BlobPhysics_Script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlobPhysics_Script : MonoBehaviour {
+	
+	public GameObject texturePlane;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Mesh mesh = texturePlane.GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		int i = 0;
+        while (i < vertices.Length) {
+            vertices[i] += Vector3.up * Time.deltaTime;
+            i++;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+	}
+}
