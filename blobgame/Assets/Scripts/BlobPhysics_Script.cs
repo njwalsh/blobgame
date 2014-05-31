53c479cda24d7e544c206b061e1e2b46c8cc10e2
njwalsh <nickjameswalsh1@gmail.com>
2014-05-30 22:28:35 -0700

    blob anim proto

--- blobgame/Assets/Scripts/BlobPhysics_Script.cs
+++ blobgame/Assets/Scripts/BlobPhysics_Script.cs
@@ -11,10 +11,17 @@ public class BlobPhysics_Script : MonoBehaviour {
 	public GameObject node4;
 	public GameObject node5;
 	public GameObject node6;
+	
+	private Vector3 node1Pos;
+	
+	Vector3[] verticesOrg;
 
 	// Use this for initialization
 	void Start () {
-	
+		node1Pos = node1.transform.localPosition;
+		
+		Mesh mesh = texturePlane.GetComponent<MeshFilter>().mesh;
+		verticesOrg = mesh.vertices;
 	}
 	
 	// Update is called once per frame
@@ -23,24 +30,10 @@ public class BlobPhysics_Script : MonoBehaviour {
 		Vector3[] vertices = mesh.vertices;
 		int i = 0;
         while (i < vertices.Length) {
-			float dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node1.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node1.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node1.transform.position.z), 2f));
-			dist = Mathf.Clamp(dist, 0, 1)/10f;
-            vertices[i] += node1.transform.position * dist * Time.deltaTime;
-			dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node2.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node2.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node2.transform.position.z), 2f));
-			dist = Mathf.Clamp(dist, 0, 1)/10f;
-			vertices[i] += node2.transform.position * dist * Time.deltaTime;
-			dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node3.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node3.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node3.transform.position.z), 2f));
-			dist = Mathf.Clamp(dist, 0, 1)/10f;
-			vertices[i] += node3.transform.position * dist * Time.deltaTime;
-			dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node4.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node4.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node4.transform.position.z), 2f));
-			dist = Mathf.Clamp(dist, 0, 1)/10f;
-			vertices[i] += node4.transform.position * dist * Time.deltaTime;
-			dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node5.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node5.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node5.transform.position.z), 2f));
-			dist = Mathf.Clamp(dist, 0, 1)/10f;
-			vertices[i] += node5.transform.position * dist * Time.deltaTime;
-			dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node6.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node6.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node6.transform.position.z), 2f));
-			dist = Mathf.Clamp(dist, 0, 1)/10f;
-			vertices[i] += node6.transform.position * dist * Time.deltaTime;
+			float dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node1.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node1.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node1.transform.position.z), 2f))/10f;
+            vertices[i] = vertices[i] /*+ (node1.transform.localPosition - node1Pos) * Time.deltaTime*/;
+			Vector3 disp = node1.transform.localPosition - node1Pos;
+			Debug.Log(disp);
             i++;
         }
         mesh.vertices = vertices;
