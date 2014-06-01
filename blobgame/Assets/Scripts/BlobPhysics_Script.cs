236aad621025ccbf7bf3ded0ca36c98f7ccba445
njwalsh <nickjameswalsh1@gmail.com>
2014-05-31 22:56:51 -0700

    blob anim physics

--- blobgame/Assets/Scripts/BlobPhysics_Script.cs
+++ blobgame/Assets/Scripts/BlobPhysics_Script.cs
@@ -12,28 +12,31 @@ public class BlobPhysics_Script : MonoBehaviour {
 	public GameObject node5;
 	public GameObject node6;
 	
-	private Vector3 node1Pos;
+	Vector3[] oldVertices;
+	Vector3[] oldNodePositions;
 	
-	Vector3[] verticesOrg;
+	GameObject[] nodes;
 
 	// Use this for initialization
 	void Start () {
-		node1Pos = node1.transform.localPosition;
-		
 		Mesh mesh = texturePlane.GetComponent<MeshFilter>().mesh;
-		verticesOrg = mesh.vertices;
+		oldVertices = mesh.vertices;
+		
+		nodes = new GameObject[6] {node1, node2, node3, node4, node5, node6};
+		oldNodePositions = new Vector3[6] {node1.transform.position, node2.transform.position, node3.transform.position, node4.transform.position, node5.transform.position, node6.transform.position};
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame0
 	void Update () {
 		Mesh mesh = texturePlane.GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
 		int i = 0;
         while (i < vertices.Length) {
-			float dist = Mathf.Sqrt(Mathf.Pow((vertices[i].x - node1.transform.position.x), 2f) + Mathf.Pow((vertices[i].y - node1.transform.position.x), 2f) + Mathf.Pow((vertices[i].z - node1.transform.position.z), 2f))/10f;
-            vertices[i] = vertices[i] /*+ (node1.transform.localPosition - node1Pos) * Time.deltaTime*/;
-			Vector3 disp = node1.transform.localPosition - node1Pos;
-			Debug.Log(disp);
+			//for(int x = 0; x < 6; x++){
+				//float dist = Mathf.Sqrt(Mathf.Pow((oldVertices[i].x - nodes[x].transform.position.x), 2) + Mathf.Pow((oldVertices[i].y - nodes[x].transform.position.y), 2) + Mathf.Pow((oldVertices[i].z - nodes[x].transform.position.z), 2));
+				//Vector3 nodeDisplacement = nodes[x].transform.position - oldNodePositions[x];
+				vertices[i] = vertices[i] /*+ (nodeDisplacement * dist)) */* Time.deltaTime;
+			//}
             i++;
         }
         mesh.vertices = vertices;
