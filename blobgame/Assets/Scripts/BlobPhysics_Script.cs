bddd027b203d4de7c55602cdc7a3b5e22f5fb995
njwalsh <nickjameswalsh1@gmail.com>
2014-06-01 22:26:15 -0700

    work on blob anim prototype

--- blobgame/Assets/Scripts/BlobPhysics_Script.cs
+++ blobgame/Assets/Scripts/BlobPhysics_Script.cs
@@ -23,7 +23,7 @@ public class BlobPhysics_Script : MonoBehaviour {
 		oldVertices = mesh.vertices;
 		
 		nodes = new GameObject[6] {node1, node2, node3, node4, node5, node6};
-		oldNodePositions = new Vector3[6] {node1.transform.position, node2.transform.position, node3.transform.position, node4.transform.position, node5.transform.position, node6.transform.position};
+		oldNodePositions = new Vector3[6] {node1.transform.localPosition, node2.transform.localPosition, node3.transform.localPosition, node4.transform.localPosition, node5.transform.localPosition, node6.transform.localPosition};
 	}
 	
 	// Update is called once per frame0
@@ -32,11 +32,13 @@ public class BlobPhysics_Script : MonoBehaviour {
 		Vector3[] vertices = mesh.vertices;
 		int i = 0;
         while (i < vertices.Length) {
-			//for(int x = 0; x < 6; x++){
-				//float dist = Mathf.Sqrt(Mathf.Pow((oldVertices[i].x - nodes[x].transform.position.x), 2) + Mathf.Pow((oldVertices[i].y - nodes[x].transform.position.y), 2) + Mathf.Pow((oldVertices[i].z - nodes[x].transform.position.z), 2));
-				//Vector3 nodeDisplacement = nodes[x].transform.position - oldNodePositions[x];
-				vertices[i] = vertices[i] /*+ (nodeDisplacement * dist)) */* Time.deltaTime;
-			//}
+			for(int x = 0; x < 6; x++){
+			//int x  = 1;
+				float dist = Mathf.Sqrt(Mathf.Pow((oldVertices[i].x - nodes[x].transform.localPosition.x), 2) + Mathf.Pow((oldVertices[i].y - nodes[x].transform.localPosition.y), 2) + Mathf.Pow((oldVertices[i].z - nodes[x].transform.localPosition.z), 2));
+				Vector3 nodeDisplacement = nodes[x].transform.localPosition - oldNodePositions[x];
+				nodeDisplacement = new Vector3(nodeDisplacement.x, 0f, nodeDisplacement.z);
+				vertices[i] = oldVertices[i] + (nodeDisplacement);
+			}
             i++;
         }
         mesh.vertices = vertices;
