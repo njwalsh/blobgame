4fa3816dd373ae2b67ca256ecc9ea6849c1362a8
njwalsh <nickjameswalsh1@gmail.com>
2014-06-08 13:38:41 -0700

    work on blob anim physics

--- blobgame/Assets/Scripts/BlobPhysics_Script.cs
+++ blobgame/Assets/Scripts/BlobPhysics_Script.cs
@@ -27,8 +27,8 @@ public class BlobPhysics_Script : MonoBehaviour {
 		
 		originalDist = Mathf.Sqrt(Mathf.Pow((node7.transform.position.x - node1.transform.position.x), 2) + Mathf.Pow((node7.transform.position.y - node1.transform.position.y), 2) + Mathf.Pow((node7.transform.position.z - node1.transform.position.z), 2));
 		
-		nodes = new GameObject[6] {node1, node2, node3, node4, node5, node6};
-		oldNodePositions = new Vector3[6] {node1.transform.localPosition, node2.transform.localPosition, node3.transform.localPosition, node4.transform.localPosition, node5.transform.localPosition, node6.transform.localPosition};
+		nodes = new GameObject[7] {node1, node2, node3, node4, node5, node6, node7};
+		oldNodePositions = new Vector3[7] {node1.transform.localPosition, node2.transform.localPosition, node3.transform.localPosition, node4.transform.localPosition, node5.transform.localPosition, node6.transform.localPosition, node7.transform.localPosition};
 	}
 	
 	// Update is called once per frame0
@@ -39,17 +39,23 @@ public class BlobPhysics_Script : MonoBehaviour {
 		float dist = 0;
 		Vector3 nodeDisplacement = Vector3.up;
         while (i < vertices.Length) {
-			for(int x = 0; x < 6; x++){
+			for(int x = 0; x < 7; x++){
 			//int x  = 0;
 				dist = Mathf.Sqrt(Mathf.Pow((oldVertices[i].x - nodes[x].transform.localPosition.x), 2) + Mathf.Pow((oldVertices[i].y - nodes[x].transform.localPosition.y), 2) + Mathf.Pow((oldVertices[i].z - nodes[x].transform.localPosition.z), 2));
-				
+				//Debug.Log("originalDist: " + originalDist);
+				//Debug.Log("cenpos: " + node7.transform.position + "nodepos: " + nodes[x].transform.position);
 				Vector3 v = node7.transform.position - nodes[x].transform.position;
+				//Debug.Log("v: " + v);
 				Vector3 u = Vector3.Normalize(v);
+				//Debug.Log("u: " + u);
 				Vector3 result = nodes[x].transform.position + (originalDist * u);
+				//Debug.Log("result: " + result);
 				result = nodes[x].transform.position - result;
+				//Debug.Log("result: " + result);
 				nodeDisplacement = new Vector3(result.x, 0f, result.y);
+				//Debug.Log("nodeDisplacement: " + nodeDisplacement);
 				
-				vertices[i] = oldVertices[i] + (nodeDisplacement);
+				vertices[i] = oldVertices[i] + (nodeDisplacement * dist);
 			}
             i++;
         }
