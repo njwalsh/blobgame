7256ff86fc28d8d46d523b7c5f451b3a9d0132dd
njwalsh <nickjameswalsh1@gmail.com>
2014-06-11 22:54:46 -0700

    added raycast to alternative player move script

--- blobgame/Assets/Scripts/PlayerMovement_Script.cs
+++ blobgame/Assets/Scripts/PlayerMovement_Script.cs
@@ -15,6 +15,14 @@ public class PlayerMovement_Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		RaycastHit hit;
+		Vector2 origin = new Vector2(this.transform.position.x, this.transform.position.y);
+		Vector2 direction = -1 * Vector2.up;
+		Debug.DrawRay(new Vector3(origin.x, origin.y, 0f), new Vector3(0f, origin.y - 5f, 0f), Color.red);
+		if(Physics2D.Raycast(origin, direction, 5f)){//add separate layer mask for player colliders
+			Debug.Log("collide");
+		}
+		
 		if(Input.GetKey(KeyCode.RightArrow)){
 			foreach(GameObject node in nodes){
 				Vector2 moveForce = new Vector2(10f, 0f);
