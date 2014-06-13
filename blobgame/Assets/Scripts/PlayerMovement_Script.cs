05d36642ded61a800a20e92e4ca5b8c83cda739c
njwalsh <nickjameswalsh1@gmail.com>
2014-06-12 23:13:01 -0700

    added grounded functionality to alt player move script

--- blobgame/Assets/Scripts/PlayerMovement_Script.cs
+++ blobgame/Assets/Scripts/PlayerMovement_Script.cs
@@ -15,12 +15,14 @@ public class PlayerMovement_Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
 		Vector2 origin = new Vector2(this.transform.position.x, this.transform.position.y);
 		Vector2 direction = -1 * Vector2.up;
 		Debug.DrawRay(new Vector3(origin.x, origin.y, 0f), new Vector3(0f, origin.y - 5f, 0f), Color.red);
-		if(Physics2D.Raycast(origin, direction, 5f)){//add separate layer mask for player colliders
-			Debug.Log("collide");
+		RaycastHit2D hit = Physics2D.Raycast(origin, direction, 5f, 1 << LayerMask.NameToLayer("Player"));
+		if(hit != null){
+			grounded = true;
+		}else{
+			grounded = false;	
 		}
 		
 		if(Input.GetKey(KeyCode.RightArrow)){
